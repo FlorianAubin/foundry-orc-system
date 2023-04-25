f10c49f0e0dd538728ebbf6407b09523300c1ad1
pi <remi.ratajczak@gmail.com>
2023-04-26 00:50:36 +0200

    feat: while true instead of watch

--- scripts/auto_pull/auto_pull.sh
+++ scripts/auto_pull/auto_pull.sh
@@ -6,10 +6,14 @@ echo "$ORCS_SYSTEM_PATH"
 remote_commit=$(git -C $ORCS_SYSTEM_PATH rev-parse origin/main)
 local_commit=$(git -C $ORCS_SYSTEM_PATH rev-parse main)
 
-if [ "$remote_commit" != "$local_commit" ]; then
-	echo "Checkout main"
-	git -C $ORCS_SYSTEM_PATH checkout main
+while true; do
 
-	echo "Pull origin"
-	git -C $ORCS_SYSTEM_PATH pull origin main
-fi
+	if [ "$remote_commit" != "$local_commit" ]; then
+		echo "Checkout main"
+		git -C $ORCS_SYSTEM_PATH checkout main
+
+		echo "Pull origin"
+		git -C $ORCS_SYSTEM_PATH pull origin main
+	fi
+	sleep 30
+done
