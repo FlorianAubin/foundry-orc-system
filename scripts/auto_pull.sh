78d63a6e3cc5460898b90b7e8ef393cbe18cd591
pi <remi.ratajczak@gmail.com>
2023-04-25 22:10:08 +0200

    ci: auto pull script based on commit main hash

---
+++ scripts/auto_pull.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+remote_commit="$(git rev-parse origin/main)"
+local_commit="$(git rev-parse main)"
+
+if [ "$remote_commit" != "$local_commit" ]; then
+	echo "Checkout main"
+	git checkout main
+
+	echo "Pull origin"
+	git pull origin main
+fi
